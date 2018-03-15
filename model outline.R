ef1e6b17a52b9a3a592f58aa1952f6b199c16cd9
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2018-03-16 09:51:07 +1100

    model outline

---
+++ model outline.R
@@ -0,0 +1,58 @@
+#####
+
+#Can you read that?
+# resistance = 5
+# Given data set
+# landscape
+# one metapopulation
+# individuals move around according to resistance values ofr XXX generations
+# e.g. pairwise FSt between subpopulations
+
+
+#### ABC landscape genetics
+
+#0. select values of  parameters (resistance) [=resABC] [from prior]
+#1. Initiate a landscape with resABC to calculate a distance matrix
+#2. run our model 
+#3. compare our output (summary statistic e.g. pairwise Fst) and check how close it is to the "real" value
+#4. mode of distribution
+
+#run this 100000x 
+
+
+#Scenarios?
+# continuous vs discrete landscapes
+# local barriers adding
+# small scale versus larger scale?
+# sample size (# subpopulations, how strong is the restistance, # loci)
+# different sizes subpopulations
+# compare landscape resistance algorithm 
+# dispersal, population density
+
+#How to decide? 
+#1. Read, find simulation literature
+#2. Predict some relationships
+#3. do some test runs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
